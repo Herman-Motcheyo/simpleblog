e3ef84b10fbcc15b6b08d4b0235d3406c8e62659
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2024-02-19 22:42:51 +0100

    add basic change of files

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,36 @@ import './App.css';
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import Single from './pages/Single';
+import Footer from './components/Footer';
+
+const Layout = () => {  
+return(
+  <>
+    <Home/>
+    <Outlet/>
+    <Footer/>
+  </>
+)  
+}
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is home!</div>,
+    element: <Layout/>,
+    children:[
+      {path:"/", element:<Home/>},
+      {path:"/single", element:<Single/>},
+      {path:"/write", element:<Write/>},
+    ]
   },
   {
     path: "/register",
@@ -29,5 +50,6 @@ const  App = ()=> {
     </div>
   );
 }
+
 
 export default App;
